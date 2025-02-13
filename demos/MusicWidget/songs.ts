bcd348a1c9d22cf64e378fa45c71c3752fbcf184
addysrii <149177246+addysrii@users.noreply.github.com>
2025-02-13 15:34:10 +0530

    Update songs.ts

--- demos/MusicWidget/songs.ts
+++ demos/MusicWidget/songs.ts
@@ -3,6 +3,7 @@ export type Song = {
   title: string
   artist: string
   image: string
+  url: string
 }
 
 export const songs: Song[] = [
@@ -10,150 +11,176 @@ export const songs: Song[] = [
     id: 'dhalti-rahe',
     title: 'Dhalti Rahe',
     artist: 'Twin Strings',
-    image:"https://i.scdn.co/image/ab67616d0000b273714cc0b3d7277d8db6840b34"
+    image:"https://i.scdn.co/image/ab67616d0000b273714cc0b3d7277d8db6840b34",
+    url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438829/dhalti-rahe_uryigo.mp3"
+    
   },
   {
     id: 'tainu-khabar-nahi',
     title: 'Tainu Khabar Nahi',
     artist: 'Sachin-Jigar, Arijit Singh',
-    image:"https://i.scdn.co/image/ab67616d00001e0267a876fee5cd00ed0f395a9e"
+    image:"https://i.scdn.co/image/ab67616d00001e0267a876fee5cd00ed0f395a9e",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438850/tainu-khabar-nahi_j6p8pn.mp3"
   },
   {
     id: 'tum-ho',
     title: 'Tum Ho',
     artist: 'A.R. Rahman, Mohit Chauhan',
-    image: "https://i.scdn.co/image/ab67616d0000b273869a9b15675b514a06d2c049"
+    image: "https://i.scdn.co/image/ab67616d0000b273869a9b15675b514a06d2c049",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438853/tum-ho_iciirs.mp3"
   },
   {
     id: 'ishq',
     title: 'Ishq',
     artist: 'Amir Ameer, Faheem Abdullah',
-    image: "https://i.scdn.co/image/ab67616d0000b273e1f7b2534c5c1fd5d9f81213"
+    image: "https://i.scdn.co/image/ab67616d0000b273e1f7b2534c5c1fd5d9f81213",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438834/ishq_mfkayt.mp3"
   },
   {
     id: 'tere-bina',
     title: 'Tere Bina',
     artist: 'Arijit Singh, Priya Saraiya',
-    image: "https://i.scdn.co/image/ab67616d0000b2731d8a12d792f9528b737a3ea5"
+    image: "https://i.scdn.co/image/ab67616d0000b2731d8a12d792f9528b737a3ea5",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438853/tere-bina_sp8twa.mp3"
   },
   {
     id: 'apna-bana-le',
     title: 'Apna Bana Le',
     artist: 'Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya',
-    image: "https://i.scdn.co/image/ab67616d0000b273b85b4e8fb6ba961aedfde386"
+    image: "https://i.scdn.co/image/ab67616d0000b273b85b4e8fb6ba961aedfde386",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438829/apna-bana-le_ionfs4.mp3"
   },
   {
     id: 'jashn-e-bahaara',
     title: 'Jashn-E-Bahaara',
     artist: 'A.R. Rahman, Javed Ali',
-    image: "https://i.scdn.co/image/ab67616d0000b27346131d4d7919c5cacd15c7d3"
+    image: "https://i.scdn.co/image/ab67616d0000b27346131d4d7919c5cacd15c7d3",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438835/jashn-e-bahaara_wdly1b.mp3"
   },
   {
     id: 'guzarish',
     title: 'Guzarish',
     artist: 'Javed Ali',
-    image: "https://i.scdn.co/image/ab67616d0000b2735b7ba7da308af447f48b9943"
+    image: "https://i.scdn.co/image/ab67616d0000b2735b7ba7da308af447f48b9943",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438831/guzarish_vfep4t.mp3"
   },
   {
     id: 'perfect',
     title: 'Perfect',
     artist: 'Ed Sheeran',
-    image: "https://i.scdn.co/image/ab67616d0000b273ba5db46f4b838ef6027e6f96"
+    image: "https://i.scdn.co/image/ab67616d0000b273ba5db46f4b838ef6027e6f96",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438840/perfect_gywyys.mp3"
   },
   {
     id: 'maan-meri-jaan',
     title: 'Maan Meri Jaan',
     artist:'King',
-    image: 'https://i.scdn.co/image/ab67616d0000b27337f65266754703fd20d29854'
+    image: 'https://i.scdn.co/image/ab67616d0000b27337f65266754703fd20d29854',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438839/maan-meri-jaan_cclkrm.mp3"
   },
   {
     id: 'ratiyaan',
     title: 'Ratiyaan',
     artist: 'Hansika Pareek, Sagnik Kolay, Soham Majumdar',
-    image: 'https://i.scdn.co/image/ab67616d0000b273e248dcc39f61806135554dbb'
+    image: 'https://i.scdn.co/image/ab67616d0000b273e248dcc39f61806135554dbb',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438846/ratiyaan_ywcgds.mp3"
   },
   {
     id: 'haan-ke-haan',
     title: 'Haan Ke Haan',
     artist: 'Sohail Sen, Monali Thakur, Kausar Munir',
-    image: 'https://i.scdn.co/image/ab67616d0000b27328d1993af511ae93aa60b22a'
+    image: 'https://i.scdn.co/image/ab67616d0000b27328d1993af511ae93aa60b22a',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438840/haan-ke-haan_jgibrh.mp3"
   },
   {
     id: 'tum-kya-mile',
     title: 'Tum Kya Mile',
     artist: 'Pritam, Arijit Singh, Shreya Ghoshal, Amitabh Bhattacharya',
-    image: 'https://i.scdn.co/image/ab67616d0000b27327725fe05e8b7a4e99cd15da'
+    image: 'https://i.scdn.co/image/ab67616d0000b27327725fe05e8b7a4e99cd15da',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438851/tum-kya-mile_a79pnv.mp3"
   },
   {
     id: 'raanjhan',
     title: 'Raanjhan (From "Do Patti")',
     artist: 'Sachet-Parampara, Parampara Tandon, Kausar Munir',
-    image: 'https://i.scdn.co/image/ab67616d0000b273773c5f60bcb309ef8802e4ef'
+    image: 'https://i.scdn.co/image/ab67616d0000b273773c5f60bcb309ef8802e4ef',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438842/raanjhan_cjrtjf.mp3"
   },
   {
     id: 'ranjha-ranjha',
     title: 'Ranjha Ranjha',
     artist: 'Rekha Bhardwaj, Javed Ali',
-    image: 'https://i.scdn.co/image/ab67616d0000b2734000b28e1e6d38fff476f095'
+    image: 'https://i.scdn.co/image/ab67616d0000b2734000b28e1e6d38fff476f095',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438846/ranjha-ranjha_l0hhys.mp3"
   },
   {
     id: 'sajni',
     title: 'Sajni',
     artist: 'Arijit Singh, Ram Sampath, Prashant Pandey',
-    image: 'https://i.scdn.co/image/ab67616d0000b273d5f4378b1ffc9119fdc7306d'
+    image: 'https://i.scdn.co/image/ab67616d0000b273d5f4378b1ffc9119fdc7306d',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438844/sajni_pimf72.mp3"
   },
   {
     id: 'sukoon-mila',
     title: 'Sukoon Mila',
     artist: 'Arijit Singh',
-    image: 'https://i.scdn.co/image/ab67616d0000b273502837886569cbec6eeab8f6'
+    image: 'https://i.scdn.co/image/ab67616d0000b273502837886569cbec6eeab8f6',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438829/dhalti-rahe_uryigo.mp3"
   },
   {
     id: 'khuda-bhi',
     title: 'Khuda Bhi',
     artist: 'Tony Kakkar, Mohit Chauhan',
-    image: 'https://i.scdn.co/image/ab67616d0000b2730dc03da31b9a63b84f9e50eb'
+    image: 'https://i.scdn.co/image/ab67616d0000b2730dc03da31b9a63b84f9e50eb',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438846/sukoon-mila_looqsp.mp3"
   },
   {
     id:'kaise-mujhe',
     title: 'Kaise Mujhe',
     artist: 'Benny Dayal, Shreya Ghoshal',
-    image: "https://i.scdn.co/image/ab67616d0000b2735b7ba7da308af447f48b9943"
+    image: "https://i.scdn.co/image/ab67616d0000b2735b7ba7da308af447f48b9943",
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438838/kaise-mujhe_knznlq.mp3"
   },
   {
     id: 'bas-ek-baar',
     title: 'Bas Ek Baar',
     artist: 'Soham Naik, Anurag Saikia',
-    image: 'https://i.scdn.co/image/ab67616d0000b273265f746066fad41150f664c2'
+    image: 'https://i.scdn.co/image/ab67616d0000b273265f746066fad41150f664c2',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438832/bas-ek-baar_wvhlga.mp3"
   }, 
   {
     id: 'samjho-na',
     title: 'Samjho Na',
     artist: 'Aditya Rikhari',
-    image: 'https://i.scdn.co/image/ab67616d0000b27361589325f2ee1797c6527512'
+    image: 'https://i.scdn.co/image/ab67616d0000b27361589325f2ee1797c6527512',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438844/samjho-na_ivdkph.mp3"
   },
   {
     id:'te-amo',
     title: 'Te Amo',
     artist: 'Pritam, Ash King, Sunidhi Chauhan',
-    image: 'https://i.scdn.co/image/ab67616d0000b273e39388ba8eadf58476135087'
+    image: 'https://i.scdn.co/image/ab67616d0000b273e39388ba8eadf58476135087',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438849/te-amo_xzpzx4.mp3"
   },
   {
     id: 'soniyo',
     title: 'Soniyo',
     artist: 'Sonu Nigam, Shreya Ghoshal',
-    image: 'https://i.scdn.co/image/ab67616d0000b273bf8dad28cba0f3680ee29364'
+    image: 'https://i.scdn.co/image/ab67616d0000b273bf8dad28cba0f3680ee29364',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438852/soniyo_rb2wrx.mp3"
   },
   {
     id: 'main-kho-gaya',
     title: 'Main Kho Gaya',
     artist: 'Kushagra, Showkidd',
-    image: 'https://i.scdn.co/image/ab67616d0000b273d351cc37d32c5af7e6ff5dec'
+    image: 'https://i.scdn.co/image/ab67616d0000b273d351cc37d32c5af7e6ff5dec',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438839/main-kho-gaya_rgwjjx.mp3"
   },
   {
     id: 'baawra',
     title: 'Baawra',
     artist: 'Kushagra, Tanishka Bahl',
-    image: 'https://i.scdn.co/image/ab67616d0000b27384723b3228ffb6b6b661e277'
+    image: 'https://i.scdn.co/image/ab67616d0000b27384723b3228ffb6b6b661e277',
+     url: "https://res.cloudinary.com/dnnl72vrp/video/upload/v1739438830/baawra_xggape.mp3"
   }
 ].reverse()
